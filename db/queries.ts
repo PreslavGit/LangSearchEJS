2cb393b4a5977f5f88fbb608e302f480f778b1b9
Preslav <preslavk1201@gmail.com>
2023-03-18 18:03:54 +0200

    Added better error handling for lang queries

--- db/queries.ts
+++ db/queries.ts
@@ -1,4 +1,5 @@
 const pool = require('./pool')
+import { FormData } from "../controllers/AddLang"
 
 type lang = {
     id: number,
@@ -19,25 +20,35 @@ export async function getLangData(): Promise<lang[]> {
     return data;
 }
 
-export async function getLangID(lang: String): Promise<number>{
+export async function getLangID(lang: String): Promise<number | null>{
     let id
     try {
         [id] = await pool.query("SELECT id FROM langs WHERE lang=? LIMIT 1", [lang])
+        if(id.length === 0){throw "No language found"}
     } catch (err) {
         console.log(err);
+        return null;
     }
     return id[0].id;
 }
 
-export async function getLangParadigms(lang: string): Promise<String[]>{
+export async function getLangParadigms(lang: string): Promise<String[] | null>{
     let data = []
     let paradigms = []
     try{
         let id = await getLangID(lang)
+        if(id === null){throw "No language found"}
         data = await pool.query("SELECT p.paradigm, l.lang FROM langparadigms lp INNER JOIN langs l ON l.id = lp.langID INNER JOIN paradigms p ON p.id = lp.paradigmID WHERE l.id = ?", [id]) 
     }catch(err){
         console.log(err);
+        return null;
     }
     paradigms = data[0]
     return paradigms.map((d: any) => d.paradigm)
+}
+
+export async function insertLang(data: FormData){
+    console.log(`Insert ${data.name}, ${data.icon}, ${data.popularity}, ${data.performance} into langs `);
+    console.log(`Insert ${data.keywords} into keywords `);
+    
 }
